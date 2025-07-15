154561860930d06c835aaf6d35dd4936a75004cc
ColacioppoGennaro <47028896+ColacioppoGennaro@users.noreply.github.com>
2025-07-15 19:00:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+app.all('*', async (req, res) => {
+  console.log('Arrivata richiesta:', req.method, req.originalUrl, req.body);
+
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 import express from 'express';
 import cors from 'cors';
